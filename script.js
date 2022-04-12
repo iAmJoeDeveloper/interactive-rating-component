4e76c8f4d85fbc51edcd244ad9890a19c818a2fe
Joe Suarez <joesuarez.developer@gmail.com>
2022-04-12 17:43:51 -0400

    starting

---
+++ script.js
@@ -0,0 +1,27 @@
+// let button = document.getElementsByTagName('input')
+let ratingButtons = document.querySelectorAll('.container-numbers__button')
+let score = document.getElementById('card2__score')
+let card1 = document.getElementById('card1')
+let card2 = document.getElementById('card2')
+let submitBtn = document.getElementById('submit')
+let value = 0
+
+let handleHover = (event) => {
+	ratingButtons.forEach((element) => {
+		element.classList.remove('selected')
+	})
+
+	event.target.classList.add('selected')
+
+	value = event.target.textContent
+}
+
+submitBtn.addEventListener('click', function () {
+	score.textContent = value
+	card1.classList.add('card--hide')
+	card2.classList.remove('card--hide')
+})
+
+ratingButtons.forEach((element) => {
+	element.addEventListener('click', handleHover)
+})
