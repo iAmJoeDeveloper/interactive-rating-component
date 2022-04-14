95ca767c852182b129ae80e8bc2b07566e7e51af
iamjoedeveloper <joesuarez.developer@gmail.com>
2022-04-14 18:41:06 -0400

    Avoid submitting the form without having checked an option

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-// let button = document.getElementsByTagName('input')
 let ratingButtons = document.querySelectorAll('.container-numbers__button')
 let score = document.getElementById('card2__score')
 let card1 = document.getElementById('card1')
 let card2 = document.getElementById('card2')
 let submitBtn = document.getElementById('submit')
 let value = 0
+
+submitBtn.addEventListener('submit', function (event) {
+	event.preventDefault()
+})
+
+let send = () => {
+	submitBtn.addEventListener('click', function () {
+		score.textContent = value
+		card1.classList.add('card--hide')
+		card2.classList.remove('card--hide')
+	})
+}
 
 let handleHover = (event) => {
 	ratingButtons.forEach((element) => {
@@ -14,13 +25,8 @@ let handleHover = (event) => {
 	event.target.classList.add('selected')
 
 	value = event.target.textContent
+	send()
 }
-
-submitBtn.addEventListener('click', function () {
-	score.textContent = value
-	card1.classList.add('card--hide')
-	card2.classList.remove('card--hide')
-})
 
 ratingButtons.forEach((element) => {
 	element.addEventListener('click', handleHover)
